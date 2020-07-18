50666069fdf042749ed09a8144929aa31a845896
BA_NANA <798998087@qq.com>
2020-07-18 14:33:25 +0800

    Create 3.无重复字符的最长子串.cpp

---
+++ 3.无重复字符的最长子串.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+/**
+ * LeetCode
+ * 2.两数相加
+ * https://leetcode-cn.com/u/banana798/
+ */
+
+int lengthOfLongestSubstring(char * s){
+    int tem[150]={0};
+    int len = 0,i=1,result=0,tag=0;
+    while(*s != '\0'){
+        int a = s[0];
+        if(tem[a]==NULL || tem[a]<tag){
+            tem[a] = i; //标记该字符存在
+            len++;
+        }else{
+            result=len>result?len:result;
+            tag=tem[a];
+            len=i-tag-1;
+            tem[a]=NULL;
+            continue;
+        }
+        s++;
+        i++;
+    }
+    return len>result?len:result;
+}
+
+int main(){
+    char *s="pwwkew";
+    printf("%d",lengthOfLongestSubstring(s));
+}
